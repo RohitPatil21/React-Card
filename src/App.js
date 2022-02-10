d41b09507c79a4d1188b41e697f0440112f6e30a
Rohit <rohitpatil7@yahoo.com>
2022-02-10 17:40:29 +0530

    New Cards

--- src/App.js
+++ src/App.js
@@ -5,30 +5,26 @@ import './App.css';
 class App extends Component {
 	constructor() {
 		super()
+
 		this.state = {
-			india : `India, officially the Republic of India (Hindi: Bhārat Gaṇarājya),[24] is a country in South Asia. It is the seventh-largest country by area, the second-most populous country, and the most populous democracy in the world. Bounded by the Indian Ocean on the south, the Arabian Sea on the southwest, and the Bay of Bengal on the southeast, it shares land borders with Pakistan to the west;[f] China, Nepal, and Bhutan to the north; and Bangladesh and Myanmar to the east. In the Indian Ocean, India is in the vicinity of Sri Lanka and the Maldives; its Andaman and Nicobar Islands share a maritime border with Thailand, Myanmar and Indonesia.`,
-
-			china : `China (Chinese: 中国; pinyin: Zhōngguó), officially the People's Republic of China (PRC; Chinese: 中华人民共和国; pinyin: Zhōnghuá Rénmín Gònghéguó), is a country in East Asia. It is the world's most populous country, with a population of more than 1.4 billion. China spans five geographical time zones and borders 14 different countries, the second most of any country in the world after Russia. Covering an area of approximately 9.6 million square kilometers (3,700,000 sq mi), it is the world's third or fourth largest country.[i] The country consists of 23 provinces,[j] five autonomous regions, four municipalities, and two Special Administrative Regions (Hong Kong and Macau). The national capital is Beijing.`,
-
-			japan : `Japan (Japanese: 日本, Nippon or Nihon,[nb 1] and formally 日本国)[nb 2] is an island country in East Asia. It is situated in the northwest Pacific Ocean, and is bordered on the west by the Sea of Japan, while extending from the Sea of Okhotsk in the north toward the East China Sea and Taiwan in the south. Japan is a part of the Ring of Fire, and spans an archipelago of 6852 islands covering 377,975 square kilometers (145,937 sq mi); the five main islands are Hokkaido, Honshu (the "mainland"), Shikoku, Kyushu, and Okinawa. Tokyo is the nation's capital and largest city; other major cities include Yokohama, Osaka, Nagoya, Sapporo, Fukuoka, Kobe, and Kyoto
-			Japan is the eleventh-most populous country in the world, as well as one of the most densely populated and urbanized. About three-fourths of the country's terrain is mountainous, concentrating its population of 125.44 million on narrow coastal plains. Japan is divided into 47 administrative prefectures and eight traditional regions. The Greater Tokyo Area is the most populous metropolitan area in the world, with more than 37.4 million residents.`,
-
-			america : `The United States of America (U.S.A. or USA), commonly known as the United States (U.S. or US) or America, is a country primarily located in North America. It consists of 50 states, a federal district, five major unincorporated territories, 326 Indian reservations, and some minor possessions.[h] At nearly 3.8 million square miles (9.8 million square kilometers), it is the world's third- or fourth-largest country by geographic area.[c] The United States shares significant land borders with Canada to the north and Mexico to the south as well as limited maritime borders with the Bahamas, Cuba, and Russia.[22] With a population of more than 331 million people, it is the third most populous country in the world. The national capital is Washington, D.C., and the most populous city and financial center is New York City.`
+			image1: "https://sportnewspedia.files.wordpress.com/2022/02/123187883_gettyimages-13694112811.jpg",
+			image2 : "https://cdn.ghanasoccernet.com/2022/02/6202efdc6e184.jpg",
+			image3 : "https://static.independent.co.uk/2022/02/08/16/newFile-20.jpg?quality=75&width=990&auto=webp&crop=982:726,smart"
 		}
 	}
 
 	render() {
-		return(
-			<div className='card-container'>
-				<Card heading={"India"} body={this.state.india} />
+		return (
+			<div className='container'>
+				<h2 className='title'>Sport</h2>
+				<div className='card-wrapper'>
+					<Card imageUrl={this.state.image1} heading={"Rodriguez strike denies Man Utd"} news={"On that occasion, they were denied two goals in the first half, while Nick Pope made some excellent saves to deny."} game_name={"Football"} />
 
-				<Card heading={"China"} body={this.state.china} />
+					<Card imageUrl={this.state.image2} heading={"Newcastle beat fellow strugglers Everton "} news={"Heitz added: As one of the most accomplished Swiss athletes of his generation, Xherdan joins us at a very important time for the club."} game_name={"Football"} />
 
-				<Card heading={"Japan"} body={this.state.japan} />
-
-				<Card heading={"America"} body={this.state.america} />
+					<Card imageUrl={this.state.image3} heading={"Anderson & Broad left out by England"} news={"Fast bowlers Anderson and Broad have taken a combined 1,177 wickets in Test cricket but were left out of the 16-man squad by a selection panel comprising of interim."} game_name={"Cricket"} />
+				</div>
 			</div>
-			
 		)
 	}
 }
--- src/Card.js
+++ src/Card.js
@@ -9,14 +9,11 @@ class Card extends Component {
 
     render() {
         return(
-            <div className='card-wrapper'>
-                <h1 className='card-heading'>
-                    {this.props.heading}
-                </h1>
-
-                <div className='card-body'>
-                    {this.props.body}
-                </div>
+            <div className='card'>
+                <img ref="image" src={this.props.imageUrl} className="image" />
+                <h2 className='heading'> {this.props.heading} </h2>
+                <p className='news'> {this.props.news} </p>
+                <p className='game_name'> {this.props.game_name} </p>
             </div>
         )
     }
